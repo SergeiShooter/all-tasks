923a485889d8cc514395660488ef31aac26709ae
Sergei Strelkov <servanstrelok@gmail.com>
2023-03-13 00:51:51 +0300

    2 week

--- homework3/index.php
+++ homework3/index.php
@@ -1,1 +1,20 @@
 <?php
+require "src/functions.php";
+
+echo task4();
+echo '<br>';
+$data = json_decode(file_get_contents('users.json'), true);
+//var_dump($data);
+
+$result = [];
+$age_sum = 0;
+foreach ($data as $name) {
+        if (isset($result[$name['name']])) {
+            $result[$name['name']] += 1;
+        } else{
+            $result[$name['name']] = 1;
+        }
+        $age_sum = $age_sum + $name['age'];
+    }
+var_dump($result);
+echo 'Средний возраст равен ' . $age_sum/ count($data);
--- homework3/src/functions.php
+++ homework3/src/functions.php
@@ -1,4 +1,21 @@
 <?php
 
-function task4 ($arrUsers = [1...50]);
+function task4 ()
+{
+    $users = [];
+    $names = ['Sasha', 'Yana', 'Dima', 'Kolya', 'Sergei'];
+    for ($i = 0; $i < 50; $i++) {
+        $user = [
+            'id' => 'N' . $i,
+            'name' => $names[array_rand($names)],
+            'age' => mt_rand(18, 45)
+        ];
+        $users[] = $user;
+    }
 
+ echo   '<pre>';
+print_r ($users);
+
+file_put_contents("users.json", json_encode($users));
+
+}
